fe7de5735a85b0ef16e464a39195f4a1ff46a7f4
YanCanCode <tillett.eric@gmail.com>
2020-10-03 10:28:59 -0400

    Added POST test to add empty repo then DELETE to remove it.  Token removed for safety.

--- webapi/src/test/java/framework/httpentity/DeleteAndPost.java
+++ webapi/src/test/java/framework/httpentity/DeleteAndPost.java
@@ -1,31 +1,57 @@
 package framework.httpentity;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
 
 public class DeleteAndPost {
 
-    private static String BASE_URL = "https://api.github.com";
+
+    private final String BASE_URL = "https://api.github.com";
+    private String token = "";
     protected RestTemplate restTemplate = new RestTemplate();
 
-    //commented out until TO-DO is done
-//    @Test
-//    public void deleteSuccessful() {
-//        // Arrange
-//        HttpHeaders headers = new HttpHeaders();
-//        // TODO: find a way to hide passwords or token
-//        headers.setBasicAuth("YanCanCode", "notCurrentlyAPassword");
-//        headers.setBearerAuth("token");
-//
-//        HttpEntity<String> request = new HttpEntity<>(headers);
-//
-//        // Act
-//        ResponseEntity response = restTemplate
-//                .exchange(BASE_URL + "/repos/YanCanCode/TestAdd",
-//                HttpMethod.DELETE, request, String.class);
-//        // Assert
-//        int responseCode = response.getStatusCodeValue();
-//
-//        Assertions.assertEquals(204, responseCode);
-//    }
 
+    @Test
+    public void createRepoReturns201() {
+        // Arrange
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+
+        String body = "{\"name\": \"deleteme\"}";
+
+        HttpEntity request = new HttpEntity(body, headers);
+
+        // Act
+        ResponseEntity response = restTemplate.postForEntity(BASE_URL + "/user/repos",
+                request, String.class);
+
+        // Assert
+        Assertions.assertEquals(201,response.getStatusCodeValue());
+
+    }
+
+    @Test
+    public void deleteSuccessful() {
+        // Arrange
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setBearerAuth(token);
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        // Act
+        ResponseEntity response = restTemplate
+                .exchange(BASE_URL + "/repos/YanCanCode/deleteme",
+                        HttpMethod.DELETE, request, String.class);
+
+        // Assert
+        int responseCode = response.getStatusCodeValue();
+        Assertions.assertEquals(204, responseCode);
+    }
 }
